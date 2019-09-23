0d1b5e0f3ba5c5a2b6c59e81223edadda5c78fff
unknown <josealvis91@hotmail.com>
2019-09-23 09:59:19 -0400

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
 
- let app  =  document.getElementById('app');
 
- app.innerHTML = "Everything works great. this is a webpack configuration";
- //
+class App extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <div className="row">
+                    <div className="col-sm">
+                        <h2>hello</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+//component has to be in uppercase
+render(<App />, window.document.getElementById('app'));
