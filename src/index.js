2e790d5f4f29e2c312a968f587f725a3ba5c0049
unknown <josealvis91@hotmail.com>
2019-10-13 21:05:07 -0400

    add middleware

--- src/index.js
+++ src/index.js
@@ -60,27 +60,67 @@ render(<App />, window.document.getElementById('app'));
 
 
 
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger }  from 'redux-logger';
 
-const reducer = (state, action) => {
+const userReducer = (state = {
+    user: 'jose',
+    logged: false
+}, action) => {
+
+    switch (action.type) {
+        case "USER_REDUCER_UPDATE_LOGIN":
+            state = {
+                ...state,
+                user: action.payload,
+            }
+
+            break;
+        case "USER_REDUCER_UPDATE_LOGING_STATUS":
+            state = {
+                ...state, logged: action.payload,
+            }
+            break;
+    }
+    return state;
+}
+
+const reducer = (state = {
+    result: 1,
+    lastValues: []
+}
+    , action) => {
 
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
+
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state, result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            }
             break;
     }
     return state;
 }
 
 
-const store = createStore(reducer,0);
+const store = createStore(
+    combineReducers({ reducer, userReducer }),
+    {},
+    applyMiddleware(createLogger())
+);
 
 store.subscribe(() => {
-    console.log("State Updated!!", store.getState());
+   // console.log("State Updated!!", store.getState());
 });
 
-store.dispatch({type: 'ADD',payload: 10});
-store.dispatch({type: 'ADD',payload: 10});
+store.dispatch({ type: 'ADD', payload: 10 });
+store.dispatch({ type: 'USER_REDUCER_UPDATE_LOGING_STATUS', payload: true });
+store.dispatch({ type: 'ADD', payload: 22 });
